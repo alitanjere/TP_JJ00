8429b77cca21b184d413cf4b2018c1bb664057c3
ALAN JEREMY ZOLOTOW <94643227@ort>
2024-08-20 14:38:23 -0300

    Entrega

--- obj/Debug/net8.0/TP_JJOO.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP_JJOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb7afdfd6fb090874b92a64c04604df62c3c800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a102ad98005c5cdf06a841f7d0b29b6369fdca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
